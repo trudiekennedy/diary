2d854c59126d2894ed41f1ced0226b835672236e
trudie anderson <trudiekennedy@gmail.com>
2025-04-16 16:33:52 +0100

    updating readable_entry.py

--- lib/readable_entry.py
+++ lib/readable_entry.py
@@ -3,14 +3,6 @@ class ReadableEntry():
         self.entries = []
 
     def extract(self, diary, wpm, mins):
-        # Parameters:
-        # diary: takes an instance of Diary
-        # wpm: integer representing the number of words per minute user can read
-        # mins: integer representing the number of mins the user has to read
-        # Returns:
-        # A list of entries the user should be able to read within alloted timeframe
-        # Side-effects:
-        # Adds entries to a list in the self object
         words_user_can_read = wpm * mins
 
         # finds entries where word count is less than or equal to what the use can read
